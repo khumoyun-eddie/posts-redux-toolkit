62408e13b0010be592d1fce3890c895d72b605ef
khumoyun-eddie <eddie.freelance21@gmail.com>
2022-07-17 10:00:56 +0500

    reactions added

--- src/features/post/PostsList.js
+++ src/features/post/PostsList.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postSlice';
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
+import ReactionButtons from './ReactionButtons';
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
@@ -17,6 +18,7 @@ const PostsList = () => {
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
+        <ReactionButtons post={post} />
     </article>
   ));
 
